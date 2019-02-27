c3fa7fac8074187fefd4862083a97275f06ab472
assur93 <47425249+assur93@users.noreply.github.com>
2019-02-27 15:29:33 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install python3 python3-venv python3-pip
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo useradd -rm homeassistant
+cd /srv
+sudo mkdir homeassistant
+sudo chown –R homeassistant:homeassistant homeassistant
+sudo su -s /bin/bash homeassistant
+cd /srv/homeassistant
+python3 -m venv .
+source bin/activate
+pip3 install homeassistant
+Hass
+cd
+sudo apt-get install nano
+sudo nano -w /etc/systemd/system/homeassistant.service
+sudo systemctl daemon-reload
+sudo systemctl enable homeassistant
+sudo systemctl start homeassistant
