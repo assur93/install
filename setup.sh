cc5bcb2b3114520fc74f471d0e7d2556d3a21c5c
assur93 <47425249+assur93@users.noreply.github.com>
2019-02-28 13:11:07 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,13 +9,3 @@ cd /srv
 sudo mkdir homeassistant
 sudo chown homeassistant:homeassistant homeassistant
 sudo su -s /bin/bash homeassistant
-cd /srv/homeassistant
-python3 -m venv .
-source bin/activate
-pip3 install homeassistant
-# Hass
-cd
-# sudo nano -w /etc/systemd/system/homeassistant.service
-# sudo systemctl daemon-reload
-# sudo systemctl enable homeassistant
-# sudo systemctl start homeassistant
