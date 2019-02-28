6501473ff2f39f7e602b2f3d2772dbd713142e56
assur93 <47425249+assur93@users.noreply.github.com>
2019-02-28 13:30:45 +0100

    Update mosquito.sh

--- mosquito.sh
+++ mosquito.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
 cd
+mkdir mosquitto
+cd mosquitto
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+sudo apt-key add mosquitto-repo.gpg.key
+cd /etc/apt/sources.list.d/
+sudo wget http://repo.mosquitto.org/debian/mosquitto-stretch.list
+sudo apt-get update
+sudo apt-get install mosquitto
+sudo apt-get install mosquitto-clients
+cd /etc/mosquitto
+sudo mosquitto_passwd -c passwds insega
+sudo systemctl enable mosquitto.service
+sudo systemctl start mosquitto.service
