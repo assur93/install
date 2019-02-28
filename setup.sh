1fbc9bcb6f6a04b181b7c0d95ab3fb0941a52e13
assur93 <47425249+assur93@users.noreply.github.com>
2019-02-28 10:50:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt-get install nano
 sudo apt-get install python3 python3-venv python3-pip
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 sudo useradd -rm homeassistant
@@ -14,7 +15,6 @@ source bin/activate
 pip3 install homeassistant
 # Hass
 # cd
-# sudo apt-get install nano
 # sudo nano -w /etc/systemd/system/homeassistant.service
 # sudo systemctl daemon-reload
 # sudo systemctl enable homeassistant
