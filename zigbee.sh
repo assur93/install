7a68582cb4d1346bd6c4ea55f2a57adb61525e9d
assur93 <47425249+assur93@users.noreply.github.com>
2019-08-14 09:00:42 +0200

    Update zigbee.sh

--- zigbee.sh
+++ zigbee.sh
@@ -1,6 +1,6 @@
 cd
 ls /dev/ttyACM0
-sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs git make g++ gcc
 node --version
 npm --version
