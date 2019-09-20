cb3eb87e1381b4c037cf140a10633dbe74a7f750
assur93 <47425249+assur93@users.noreply.github.com>
2019-09-20 15:48:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@ cd
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install nano
-sudo apt-get install python3 python3-venv python3-pip
+sudo apt-get install npm
+sudo apt-get install python3
+sudo apt-get install python3-venv
+sudo apt-get install python3-pip
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 sudo useradd -rm homeassistant
 cd /srv
